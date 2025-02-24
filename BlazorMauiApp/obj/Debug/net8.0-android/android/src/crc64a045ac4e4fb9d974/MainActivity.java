614285d71bc3ea42f6845865ca0a472e1a77e35c
milanmogha <moghamilan@gmail.com>
2025-02-24 16:18:53 +0530

    Initial commit

---
+++ BlazorMauiApp/obj/Debug/net8.0-android/android/src/crc64a045ac4e4fb9d974/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64a045ac4e4fb9d974;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("BlazorMauiApp.MainActivity, BlazorMauiApp", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("BlazorMauiApp.MainActivity, BlazorMauiApp", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("BlazorMauiApp.MainActivity, BlazorMauiApp", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
